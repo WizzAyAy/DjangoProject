ec0b96af9f6681ef5447011450e65b3ba85a392b
Quentin Maignan <quentindu4913@hotmail.fr>
2021-12-10 16:18:47 +0100

    settings

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = [
 
 INSTALLED_APPS = [
     'paper.apps.PaperConfig',
+    'information.apps.InformationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
