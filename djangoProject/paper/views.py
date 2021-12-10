d81fd292f5c4a00f47d2115378a009556e73e6c5
Quentin Maignan <quentindu4913@hotmail.fr>
2021-12-10 15:08:09 +0100

    highchart

--- djangoProject/paper/views.py
+++ djangoProject/paper/views.py
@@ -12,3 +12,15 @@ def view(request):
     paper = get_object_or_404(Paper, pk=request.POST.get('paper'))
     context = {'paper': paper}
     return render(request, '../templates/paper.html', context)
+
+
+def graph(request):
+    context = {
+        'numberOfPaper': 12,
+        'molecules': ['molecule1', 'molecule2', 'molecule3', 'molecule4', 'molecule5', 'molecule6', 'molecule7',
+                      'molecule8'],
+        'molecules_values': [50, 10, 5, 6, 8, 50, 30, 10],
+        'vaccines': ['moderna', 'physez'],
+        'vaccines_values': [50, 5]
+    }
+    return render(request, '../templates/graph.html', context)
